c7b96f2c66e115bc099bd4020ad4ca763ec63147
Ian Lancaster <ianclancaster@gmail.com>
2017-02-13 20:11:25 -0700

    Write level4 tests

--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -9,6 +9,7 @@ const Person = require('../person').default
 describe('Elevator', () => {
   const elevator = new Elevator({ name: 'Unamed Elevator' })
   const Luke = new Person({ name: 'Luke', currentFloor: 2 })
+  const Leigh = new Person({ name: 'Leigh', currentFloor: 0 })
 
   beforeEach(() => {
     elevator.reset()
@@ -43,9 +44,9 @@ describe('Elevator', () => {
     expect(elevator.riders).to.deep.equal([])
   })
 
-  it('should correctly track key metrics when picking up multiple passangers', () => {
+  it('should correctly track key metrics when Luke goes up and Leigh goes down', () => {
     Luke.setFloor(2)
-    const Leigh = new Person({ name: 'Leigh', currentFloor: 8 })
+    Leigh.setFloor(8)
 
     elevator.requestFloor({ person: Luke, requestedFloor: 9 })
     elevator.requestFloor({ person: Leigh, requestedFloor: 0 })
@@ -54,6 +55,51 @@ describe('Elevator', () => {
     expect(elevator.status).to.equal('idle')
     expect(elevator.stopsMade).to.equal(4)
     expect(elevator.floorsTraversed).to.equal(18)
+    expect(elevator.requests).to.deep.equal([])
+    expect(elevator.riders).to.deep.equal([])
+  })
+
+  it('should correctly track key metrics when Luke goes up and Leigh goes up', () => {
+    Luke.setFloor(2)
+    Leigh.setFloor(2)
+
+    elevator.requestFloor({ person: Luke, requestedFloor: 9 })
+    elevator.requestFloor({ person: Leigh, requestedFloor: 4 })
+
+    expect(elevator.currentFloor).to.equal(4)
+    expect(elevator.status).to.equal('idle')
+    expect(elevator.stopsMade).to.equal(4)
+    expect(elevator.floorsTraversed).to.equal(18)
+    expect(elevator.requests).to.deep.equal([])
+    expect(elevator.riders).to.deep.equal([])
+  })
+
+  it('should correctly track key metrics when Luke goes down and Leigh goes down', () => {
+    Luke.setFloor(9)
+    Leigh.setFloor(8)
+
+    elevator.requestFloor({ person: Luke, requestedFloor: 3 })
+    elevator.requestFloor({ person: Leigh, requestedFloor: 0 })
+
+    expect(elevator.currentFloor).to.equal(0)
+    expect(elevator.status).to.equal('idle')
+    expect(elevator.stopsMade).to.equal(4)
+    expect(elevator.floorsTraversed).to.equal(28)
+    expect(elevator.requests).to.deep.equal([])
+    expect(elevator.riders).to.deep.equal([])
+  })
+
+  it('should correctly track key metrics when Luke goes down and Leigh goes up', () => {
+    Luke.setFloor(9)
+    Leigh.setFloor(3)
+
+    elevator.requestFloor({ person: Luke, requestedFloor: 0 })
+    elevator.requestFloor({ person: Leigh, requestedFloor: 7 })
+
+    expect(elevator.currentFloor).to.equal(7)
+    expect(elevator.status).to.equal('idle')
+    expect(elevator.stopsMade).to.equal(4)
+    expect(elevator.floorsTraversed).to.equal(25)
     expect(elevator.requests).to.deep.equal([])
     expect(elevator.riders).to.deep.equal([])
   })
